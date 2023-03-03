6a926bf511bbd9413c5ce2f20387975a97f06957
francomoraes <francoapmoraes@gmail.com>
2023-03-03 20:50:23 -0300

    added || 3000 to port listen

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
