c53588124e3ae4a474038a5d2526550fa4f76d24
francomoraes <francoapmoraes@gmail.com>
2023-03-07 20:24:57 -0300

    changed fixed port to process.env

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log(`app is running on port ${PORT}`);
