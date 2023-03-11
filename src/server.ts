c86e26a788872659c291220a975b12ca9e65e3a0
francomoraes <francoapmoraes@gmail.com>
2023-03-11 08:59:26 -0300

    changing database configs to cloud

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,10 @@ const knex = require('knex');
 const db = knex({
     client: 'pg',
     connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: '',
-        database: 'smartbrain'
+        host: 'dpg-cg3sm5ndvk4hn47amoo0-a'
+        // user: 'postgres',
+        // password: '',
+        // database: 'smartbrain'
     }
 });
 
